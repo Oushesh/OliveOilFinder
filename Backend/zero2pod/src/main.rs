cb07beca2bbb15168ebbbb42b94a275dcbb6d76b
oushesh <oushesh@gmail.com>
2026-01-07 00:20:16 +0100

    added new functions

--- Backend/zero2pod/src/main.rs
+++ Backend/zero2pod/src/main.rs
@@ -1,5 +1,28 @@
+/*
 fn main() {
     println!("Hello, world!");
 }
+*/
 
 //Our First Endpoint: A Basic health Check
+//From Book zeor-to-production-in-rust-an-introduction book from the past.
+
+
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+async fn greet(req: HttpRequest) -> impl Responder {
+let name = req.match_info().get("name").unwrap_or("World");
+format!("Hello {}!", &name)
+}
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+HttpServer::new(|| {
+App::new()
+.route("/", web::get().to(greet))
+.route("/{name}", web::get().to(greet))
+})
+.bind("127.0.0.1:8000")?
+.run()
+.await
+}
